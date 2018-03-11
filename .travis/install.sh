5284286cc01c8a3b26346102cc43094bd17f1bdf
Pine Mizune <pinemz@gmail.com>
2018-03-11 22:42:36 +0900

    fix(install.sh): install python3

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 set -eux
+
+# install pyenv
+git clone --depth 1 https://github.com/pyenv/pyenv ~/.pyenv
+PYENV_ROOT="$HOME/.pyenv"
+PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+pyenv install 3.6.4
+pyenv global 3.6.4
+pyenv rehash
+
+python -m virtualenv .venv
+source .venv/bin/activate
