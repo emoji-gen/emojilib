17d01226c67a893f5d7ff03e55efa98f21398b5d
Pine Mizune <pinemz@gmail.com>
2018-03-12 21:01:24 +0900

    fix(travis): fix build command

--- .travis/build_externals.sh
+++ .travis/build_externals.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eux
+
 cd externals/libemoji
 cmake .
 make
---
+++ .travis/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eux
+
+# setup pyenv
+PYENV_ROOT="$HOME/.pyenv"
+PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# setup venv
+VIRTUAL_ENV_DISABLE_PROMPT=true
+source .venv/bin/activate
