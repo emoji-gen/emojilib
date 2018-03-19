c2469e8d024b6725c5928e522dfd30678310fab7
Pine Mizune <pinemz@gmail.com>
2018-03-19 21:36:41 +0900

    fix(wercker): fix err

--- .wercker/run.sh
+++ .wercker/run.sh
@@ -2,8 +2,13 @@
 
 set -eux
 
-/usr/local/python-3.5/bin/python setup.py bdist_wheel
+/usr/local/python-3.5/bin/python setup.py bdist_wheel test
 cp build/lib.*/pyemoji.*.so .
+/usr/local/python-3.5/bin/python scripts/publish.py
 
-/usr/local/python-3.6/bin/python setup.py bdist_wheel
+rm -rf build
+rm -rf *.so
+
+/usr/local/python-3.6/bin/python setup.py bdist_wheel test
 cp build/lib.*/pyemoji.*.so .
+/usr/local/python-3.6/bin/python scripts/publish.py
