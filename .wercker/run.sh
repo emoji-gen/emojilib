fa35082fb0000f25a3432056e43f3b46f65a4dee
Pine Mizune <pinemz@gmail.com>
2018-03-19 21:53:33 +0900

    fix(wercker): fix clean script

--- .wercker/run.sh
+++ .wercker/run.sh
@@ -6,8 +6,7 @@ set -eux
 cp build/lib.*/pyemoji.*.so .
 /usr/local/python-3.5/bin/python scripts/publish.py
 
-rm -rf build
-rm -rf *.so
+./scripts/clean.sh
 
 /usr/local/python-3.6/bin/python setup.py bdist_wheel test
 cp build/lib.*/pyemoji.*.so .
