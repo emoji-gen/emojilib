6f0f98b90761de6b57410651e44bec191afd049b
Pine Mizune <pinemz@gmail.com>
2018-03-21 19:43:25 +0900

    style(.werker/install.sh): remove unnecessary spaces

--- .wercker/install.sh
+++ .wercker/install.sh
@@ -4,5 +4,5 @@ set -eux
 
 git submodule update --init --recursive
 
-/usr/local/python-3.5/bin/python  -m pip install -r requirements-dev.txt
-/usr/local/python-3.6/bin/python  -m pip install -r requirements-dev.txt
+/usr/local/python-3.5/bin/python -m pip install -r requirements-dev.txt
+/usr/local/python-3.6/bin/python -m pip install -r requirements-dev.txt
