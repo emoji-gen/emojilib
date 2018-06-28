86c3d78fd3753cbe804b0585ee8818b397f51dca
Pine Mizune <pinemz@gmail.com>
2018-06-28 13:51:53 +0900

    fix(.travis): add python 3.7

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,7 @@ set -eux
 
 PYTHON_35_VERSION=3.5.5
 PYTHON_36_VERSION=3.6.5
+PYTHON_37_VERSION=3.7.0
 
 # install lfs
 brew install git-lfs
@@ -15,10 +16,13 @@ mkdir -p ~/local
 git clone https://github.com/tagomoris/xbuild.git ~/local/xbuild
 ~/local/xbuild/python-install -f $PYTHON_35_VERSION ~/local/python-3.5
 ~/local/xbuild/python-install -f $PYTHON_36_VERSION ~/local/python-3.6
+~/local/xbuild/python-install -f $PYTHON_37_VERSION ~/local/python-3.7
 
 # install requirements
 ~/local/python-3.5/bin/pip3 install -r requirements-dev.txt
 ~/local/python-3.6/bin/pip3 install -r requirements-dev.txt
+~/local/python-3.7/bin/pip3 install -r requirements-dev.txt
 ~/local/python-3.5/bin/pip3 install --upgrade wheel
 ~/local/python-3.6/bin/pip3 install --upgrade wheel
+~/local/python-3.7/bin/pip3 install --upgrade wheel
 gem install gemfury --no-document
--- .travis/run.sh
+++ .travis/run.sh
@@ -2,11 +2,16 @@
 
 set -eux
 
+# 3.5
 ~/local/python-3.5/bin/python setup.py bdist_wheel install test
 ~/local/python-3.5/bin/python ./scripts/publish.py
-
 ./scripts/clean.sh
 
+# 3.6
 ~/local/python-3.6/bin/python setup.py bdist_wheel install test
 ~/local/python-3.6/bin/python ./scripts/publish.py
+./scripts/clean.sh
 
+# 3.7
+~/local/python-3.7/bin/python setup.py bdist_wheel install test
+~/local/python-3.7/bin/python ./scripts/publish.py
