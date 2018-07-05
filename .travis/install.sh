1e8e32f33d89fca7a86353680d14c26a7488bc06
Pine Mizune <pinemz@gmail.com>
2018-07-05 11:05:05 +0900

    fix(install.sh): use sudo

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,8 +12,8 @@ git lfs install
 git lfs pull
 
 # install retry
-curl https://raw.githubusercontent.com/kadwanev/retry/master/retry -o /usr/bin/retry
-chmod +x /usr/bin/retry
+sudo curl https://raw.githubusercontent.com/kadwanev/retry/master/retry -o /usr/bin/retry
+sudo chmod +x /usr/bin/retry
 
 # install python
 mkdir -p ~/local
