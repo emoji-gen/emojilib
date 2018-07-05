3d7a65831e186cb17e78c2ae5c7707e13d7f91aa
Pine Mizune <pinemz@gmail.com>
2018-07-05 10:59:12 +0900

    fix(.travis): use retry command

--- .travis/install.sh
+++ .travis/install.sh
@@ -11,6 +11,10 @@ brew install git-lfs
 git lfs install
 git lfs pull
 
+# install retry
+curl https://raw.githubusercontent.com/kadwanev/retry/master/retry -o /usr/bin/retry
+chmod +x /usr/bin/retry
+
 # install python
 mkdir -p ~/local
 git clone https://github.com/tagomoris/xbuild.git ~/local/xbuild
--- .travis/run.sh
+++ .travis/run.sh
@@ -3,15 +3,15 @@
 set -eux
 
 # 3.5
-travis_retry ~/local/python-3.5/bin/python setup.py bdist_wheel install test
-travis_retry ~/local/python-3.5/bin/python ./scripts/publish.py
+retry ~/local/python-3.5/bin/python setup.py bdist_wheel install test
+retry ~/local/python-3.5/bin/python ./scripts/publish.py
 ./scripts/clean.sh
 
 # 3.6
-travis_retry ~/local/python-3.6/bin/python setup.py bdist_wheel install test
-travis_retry ~/local/python-3.6/bin/python ./scripts/publish.py
+retry ~/local/python-3.6/bin/python setup.py bdist_wheel install test
+retry ~/local/python-3.6/bin/python ./scripts/publish.py
 ./scripts/clean.sh
 
 # 3.7
-travis_retry ~/local/python-3.7/bin/python setup.py bdist_wheel install test
-travis_retry ~/local/python-3.7/bin/python ./scripts/publish.py
+retry ~/local/python-3.7/bin/python setup.py bdist_wheel install test
+retry ~/local/python-3.7/bin/python ./scripts/publish.py
