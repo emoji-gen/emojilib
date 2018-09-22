d53af5b4d1842a6c3478302999c908f04c829820
Pine Mizune <pinemz@gmail.com>
2018-09-23 04:16:22 +0900

    fix(run.sh): support pypi

--- .wercker/run.sh
+++ .wercker/run.sh
@@ -3,15 +3,19 @@
 set -eux
 
 # 3.5
-/usr/local/python-3.5/bin/python setup.py bdist_wheel install test
-/usr/local/python-3.5/bin/python scripts/publish.py
 ./scripts/clean.sh
+/usr/local/python-3.5/bin/python setup.py bdist_wheel install test
+/usr/local/python-3.5/bin/python scripts/publish.py --target=gemfury
+/usr/local/python-3.5/bin/python scripts/publish.py --target=pypi
 
 # 3.6
-/usr/local/python-3.6/bin/python setup.py bdist_wheel install test
-/usr/local/python-3.6/bin/python scripts/publish.py
 ./scripts/clean.sh
+/usr/local/python-3.6/bin/python setup.py bdist_wheel install test
+/usr/local/python-3.6/bin/python scripts/publish.py --target=gemfury
+/usr/local/python-3.6/bin/python scripts/publish.py --target=pypi
 
 # 3.7
+./scripts/clean.sh
 /usr/local/python-3.7/bin/python setup.py bdist_wheel install test
-/usr/local/python-3.7/bin/python scripts/publish.py
+/usr/local/python-3.7/bin/python scripts/publish.py --target=gemfury
+/usr/local/python-3.7/bin/python scripts/publish.py --target=pypi
