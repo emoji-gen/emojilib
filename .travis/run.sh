9cf1137a1a3cdb8ac7d8ac73f764076808c704fa
Pine Mizune <pinemz@gmail.com>
2018-09-23 22:56:19 +0900

    fix(.travis/run.sh): set path

--- .travis/run.sh
+++ .travis/run.sh
@@ -8,16 +8,19 @@ PATH=/usr/local/opt/gnu-getopt/bin:$PATH
 ./scripts/clean.sh
 retry -- ~/local/python-3.5/bin/python setup.py bdist_wheel install test
 retry -- ~/local/python-3.5/bin/python ./scripts/publish.py --target=gemfury
-retry -- ~/local/python-3.5/bin/python ./scripts/publish.py --target=pypi
+PATH=~/local/python-3.5/bin:$PATH \
+  retry -- ~/local/python-3.5/bin/python ./scripts/publish.py --target=pypi
 
 # 3.6
 ./scripts/clean.sh
 retry -- ~/local/python-3.6/bin/python setup.py bdist_wheel install test
 retry -- ~/local/python-3.6/bin/python ./scripts/publish.py --target=gemfury
-retry -- ~/local/python-3.6/bin/python ./scripts/publish.py --target=pypi
+PATH=~/local/python-3.6/bin:$PATH \
+  retry -- ~/local/python-3.6/bin/python ./scripts/publish.py --target=pypi
 
 # 3.7
 ./scripts/clean.sh
 retry -- ~/local/python-3.7/bin/python setup.py bdist_wheel install test
 retry -- ~/local/python-3.7/bin/python ./scripts/publish.py --target=gemfury
-retry -- ~/local/python-3.7/bin/python ./scripts/publish.py --target=pypi
+PATH=~/local/python-3.7/bin:$PATH \
+  retry -- ~/local/python-3.7/bin/python ./scripts/publish.py --target=pypi
