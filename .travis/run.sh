a035ce8e7c825be3cc5f382623288ba2805177c6
Pine Mizune <pinemz@gmail.com>
2018-09-24 00:07:42 +0900

    fix(.travis): use for loop

--- .travis/run.sh
+++ .travis/run.sh
@@ -4,23 +4,10 @@ set -eu -o pipefail
 
 PATH=/usr/local/opt/gnu-getopt/bin:$PATH
 
-# 3.5
-./scripts/clean.sh
-retry -- ~/local/python-3.5/bin/python setup.py bdist_wheel install test
-retry -- ~/local/python-3.5/bin/python ./scripts/publish.py --target=gemfury
-PATH=~/local/python-3.5/bin:$PATH \
-  retry -- ~/local/python-3.5/bin/python ./scripts/publish.py --target=pypi
-
-# 3.6
-./scripts/clean.sh
-retry -- ~/local/python-3.6/bin/python setup.py bdist_wheel install test
-retry -- ~/local/python-3.6/bin/python ./scripts/publish.py --target=gemfury
-PATH=~/local/python-3.6/bin:$PATH \
-  retry -- ~/local/python-3.6/bin/python ./scripts/publish.py --target=pypi
-
-# 3.7
-./scripts/clean.sh
-retry -- ~/local/python-3.7/bin/python setup.py bdist_wheel install test
-retry -- ~/local/python-3.7/bin/python ./scripts/publish.py --target=gemfury
-PATH=~/local/python-3.7/bin:$PATH \
-  retry -- ~/local/python-3.7/bin/python ./scripts/publish.py --target=pypi
+for v in 3.5 3.6 3.7; do
+  ./scripts/clean.sh
+  retry -- ~/local/python-$v/bin/python setup.py bdist_wheel install test
+  retry -- ~/local/python-$v/bin/python ./scripts/publish.py --target=gemfury
+  PATH=~/local/python-$v/bin:$PATH \
+    retry -- ~/local/python-$v/bin/python ./scripts/publish.py --target=pypi
+done
