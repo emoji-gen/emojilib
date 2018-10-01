e2989bcae36a6b0cab5ab2ff38588a3972bab516
Pine Mizune <pinemz@gmail.com>
2018-10-01 21:50:09 +0900

    fix(.wercker): install unzip command

--- .wercker/install.sh
+++ .wercker/install.sh
@@ -4,6 +4,8 @@ set -eux -o pipefail
 
 git submodule update --init --recursive
 
+apt install unzip -y
+
 for v in 3.5 3.6 3.7; do
   /usr/local/python-$v/bin/python -m pip install auditwheel
   /usr/local/python-$v/bin/python -m pip install -r requirements-dev.txt
