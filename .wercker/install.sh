9c406270ba83cf2518df67fea5c34aa97b800f84
Pine Mizune <pinemz@gmail.com>
2018-11-07 17:52:16 +0900

    fix(requirements): use wheel 0.31.1

--- .wercker/install.sh
+++ .wercker/install.sh
@@ -9,6 +9,6 @@ apt-get -qq install -y --no-install-recommends unzip patchelf
 
 for v in 3.5 3.6 3.7; do
   /usr/local/python-$v/bin/python -m pip install auditwheel
-  /usr/local/python-$v/bin/python -m pip install --upgrade wheel
+  /usr/local/python-$v/bin/python -m pip install 'wheel==0.31.1'
   /usr/local/python-$v/bin/python -m pip install -r requirements-dev.txt
 done
