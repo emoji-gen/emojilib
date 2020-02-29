88dd731f202eff41fcd0dd8c14dc1ca69d858d62
Pine Mizune <pinemz@gmail.com>
2020-03-01 03:29:24 +0900

    fix: :innocent:

--- .wercker/install.sh
+++ .wercker/install.sh
@@ -2,12 +2,12 @@
 
 set -eux -o pipefail
 
-git submodule update --init --recursive
+git submodule update --init --recursive --jobs=$(($(nproc) * 3))
 
 apt-get update -qq
 apt-get -qq install -y --no-install-recommends unzip patchelf
 
-for v in 3.5 3.6 3.7; do
+for v in 3.6 3.7 3.8; do
   /usr/local/python-$v/bin/python -m pip install auditwheel
   /usr/local/python-$v/bin/python -m pip install 'wheel==0.31.1'
   /usr/local/python-$v/bin/python -m pip install -r requirements-dev.txt
